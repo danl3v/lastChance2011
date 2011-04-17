4e22d1da78eda007e016b102a0ae8e0d9a9ae3d6
Conrad <conrad.p.dean@gmail.com>
2011-04-17 10:48:21 -0500

    got flup, but haven't quite gotten it to run yet

--- getWebPy...uhhh...and.flup.too.sh
+++ getWebPy...uhhh...and.flup.too.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 wget http://webpy.org/static/web.py-0.34.tar.gz
+wget http://www.saddi.com/software/flup/dist/flup-1.0.2.tar.gz
 tar xf web.py-0.34.tar.gz
 mv web.py-0.34/web .
 rm -rf web.py-0.34
 rm -rf web.py-0.34.tar.gz
+tar xf flup-1.0.2.tar.gz
+mv flup-1.0.2/flup .
+rm -rf flup-1.0.2
+rm -rf flup-1.0.2.tar.gz
