24b760c7ec8f2e9216060f883f2612caeaa2a2c5
Conrad <conrad.p.dean@gmail.com>
2011-04-18 10:53:54 -0500

    added admin interface for injecting simple users.  added very crude verification code

--- LCD/main.py
+++ LCD/main.py
@@ -23,11 +23,21 @@ def getCarl():
     carl.filter("googleID =", str(users.get_current_user().user_id()))
     return carl.get()
 
+def getPerson(username):  # Need your advice, dan
+    carl = Carl.all()
+    carl.filter("carletonID =",username)
+    return carl.get()
+
+def generateVerificationCode():
+    # Dumb for now
+    return "apples"
+
 #### MODELS ####
 
 class Carl(db.Model):
     googleID = db.StringProperty()
     carletonID = db.StringProperty()
+    verificationCode = db.StringProperty()
 
 class Carl2Carl(db.Model):
     source = db.StringProperty()
@@ -40,8 +50,8 @@ class MainPage(webapp.RequestHandler):
 
         user = users.get_current_user()
         if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
+            login_url = users.create_logout_url(self.request.uri)
+            login_url_linktext = 'Logout'
             
             if isPaired():
                 pair_url = "pair"
@@ -50,8 +60,8 @@ class MainPage(webapp.RequestHandler):
                 pair_url = "pair"
                 pair_url_linktext = "You need to pair your account"
         else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+            login_url = users.create_login_url(self.request.uri)
+            login_url_linktext = 'Login'
             pair_url = ""
             pair_url_linktext = ""
 
@@ -64,8 +74,8 @@ class MainPage(webapp.RequestHandler):
         template_values = {
             'user': user,
             'admin': admin,
-            'url': url,
-            'url_linktext': url_linktext,
+            'login_url': login_url,
+            'login_url_linktext': login_url_linktext,
             'pair_url': pair_url,
             'pair_url_linktext': pair_url_linktext
         }
@@ -84,23 +94,54 @@ class Pair(webapp.RequestHandler):
             # use get() to return the carleton email id that this google account was paired to
             self.response.out.write("Your account is already paired")
         else:
-            carl = Carl()
-            carl.googleID = str(users.get_current_user().user_id())
-            carl.carletonID = self.request.get('carletonID')
-            carl.put()
-            self.response.out.write("Your account was successfully paired:<br>")
-            self.response.out.write("Carleton ID:" + carl.carletonID + "<br>")
-            self.response.out.write("GoogleID: " + carl.googleID)
+            #theCarl = Carl().all()
+            #theCarl.filter("carletonID =",self.request.get('carletonID'))
+            theCarl = getPerson(self.request.get('carletonID'))
+            self.response.out.write(theCarl.carletonID)
+            self.response.out.write(theCarl.verificationCode)
+            # uhh.. this better only be one entry.
+            if theCarl.verificationCode == self.request.get('verificationCode'):
+                theCarl.googleID = str(users.get_current_user().user_id())
+                theCarl.verificationCode = ""
+                theCarl.put()
+                self.response.out.write("Your account was successfully paired:<br>")
+                self.response.out.write("Carleton ID:" + theCarl.carletonID + "<br>")
+                self.response.out.write("GoogleID: " + theCarl.googleID)
 
 class Admin(webapp.RequestHandler):
     def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'admin.html')
+        carls = Carl.all()
+        template_values = {
+            'carls' : carls
+        }
+        path = os.path.join(os.path.dirname(__file__), 'templates/admin.html')
         self.response.out.write(template.render(path, template_values))
+
+class AddCarl(webapp.RequestHandler):  # Adding a new student to database
+    def post(self):
+        if users.is_current_user_admin():
+            from random import randint
+            admin = True
+            carl = Carl()
+            carl.carletonID = self.request.get('carletonID')
+            carl.verificationCode = str(randint(0,2**16))
+            self.response.out.write('Trying to put '+carl.carletonID+' into database.')
+            carl.put()
+            self.redirect('/admin')
+        else:
+            admin = False
+            self.response.out.write('Hey! You are not an administrator. SHAME')
+
 
 class PairCode(webapp.RequestHandler):
     def post(self):
         self.response.out.write(self.request.get('carletonID'))
+        # Lookup carletonID -> get/generate verification code
+        carletonAccount = getPerson(self.request.get('carletonID'))
+        carletonAccount.verificationCode = generateVerificationCode()
+        carletonAccount.put()
         # mail some stuff
+        '''http://code.google.com/appengine/docs/python/mail/sendingmail.html'''
         self.response.out.write("<br>Your pair code has been sent!! jk. we haven't coded that much yet.")
 
 class Preferences(webapp.RequestHandler):
@@ -135,6 +176,7 @@ class Preferences(webapp.RequestHandler):
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
                                       ('/admin', Admin),
+                                      ('/admin/addcarl', AddCarl),
                                       ('/preferences', Preferences),
                                       ('/pair', Pair),
                                       ('/sendPairCode', PairCode)],
