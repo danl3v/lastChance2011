50f77aa80f1cf2e43f27f406c8cd190be4887e4e
Daniel Levy <dlouislevy@gmail.com>
2011-04-19 14:03:51 -0500

    abstracted header and footer

--- LCD/main.py
+++ LCD/main.py
@@ -12,48 +12,68 @@ import session
 
 import mailfunction
 
+def getNavData(self):
+    user = session.get_current_user()
+    if user:
+        login_url = session.create_logout_url(self.request.uri)
+        login_url_linktext = 'Logout'
+        
+        if models.isPaired():
+            pair_url = "pair"
+            pair_url_linktext = "Unpair Your Account"
+        else:
+            pair_url = "pair"
+            pair_url_linktext = "You need to pair your account"
+    else:
+        login_url = session.create_login_url(self.request.uri)
+        login_url_linktext = 'Login'
+        pair_url = ""
+        pair_url_linktext = ""
+        
+    if session.is_current_user_admin():
+        admin = True
+    else:
+        admin = False
+
+    return {
+        'user': user,
+        'admin': admin,
+        'login_url': login_url,
+        'login_url_linktext': login_url_linktext,
+        'pair_url': pair_url,
+        'pair_url_linktext': pair_url_linktext
+        }
+
+def renderTemplate(self, template_file, template_values):
+
+    template_values = dict(getNavData(self), **template_values)
+
+    path = os.path.join(os.path.dirname(__file__), 'templates/header.html')
+    self.response.out.write(template.render(path, template_values))
+    
+    path = os.path.join(os.path.dirname(__file__), 'templates/' + template_file)
+    self.response.out.write(template.render(path, template_values))
+    
+    path = os.path.join(os.path.dirname(__file__), 'templates/footer.html')
+    self.response.out.write(template.render(path, template_values))
+
 class MainPage(webapp.RequestHandler):
     def get(self):
 
-        user = session.get_current_user()
-        if user:
-            login_url = session.create_logout_url(self.request.uri)
-            login_url_linktext = 'Logout'
-            
-            if models.isPaired():
-                pair_url = "pair"
-                pair_url_linktext = "Unpair Your Account"
-            else:
-                pair_url = "pair"
-                pair_url_linktext = "You need to pair your account"
-        else:
-            login_url = session.create_login_url(self.request.uri)
-            login_url_linktext = 'Login'
-            pair_url = ""
-            pair_url_linktext = ""
-
-  
-        if session.is_current_user_admin():
-            admin = True
-        else:
-            admin = False
-
-        template_values = {
-            'user': user,
-            'admin': admin,
-            'login_url': login_url,
-            'login_url_linktext': login_url_linktext,
-            'pair_url': pair_url,
-            'pair_url_linktext': pair_url_linktext
-        }
-
-        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, template_values))
-
+        template_values = {}
+        renderTemplate(self, 'index.html', template_values)
+        
 class Pair(webapp.RequestHandler):
     def get(self):
-        template_values = {}
+        template_values = getNavData(self)
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/header.html')
+        self.response.out.write(template.render(path, template_values))
+
         path = os.path.join(os.path.dirname(__file__), 'templates/pair.html')
+        self.response.out.write('<div id="main">' + template.render(path, template_values) + '</div>')
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/footer.html')
         self.response.out.write(template.render(path, template_values))
 
     def post(self):
@@ -105,8 +125,7 @@ class Preferences(webapp.RequestHandler):
             'carls2carls': carls2carls,
             }
 
-        path = os.path.join(os.path.dirname(__file__), 'templates/preferences.html')
-        self.response.out.write(template.render(path, template_values))
+        renderTemplate(self, 'preferences.html', template_values)
 
     def post(self):  # Haven't figured out what to do with this yet
         user = models.getCarl().carletonID  # this is its own line only because it's sort of a session-based/model operation
