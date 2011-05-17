f6e56115b2107f38b5c815f87316478b4d9641f9
Daniel Levy <dlouislevy@gmail.com>
2011-05-17 14:37:58 -0500

    added reference property to messages and renamed to threads in preparation for move

--- LCD/messages.py
+++ LCD/messages.py
@@ -4,15 +4,16 @@ import models, view, session, functions
 class Send(webapp.RequestHandler):
     def post(self):
         if session.isPaired() and session.opted_in():
-            carleton_id = session.getCarl().carletonID
-            if not functions.get_user_by_CID(self.request.get("to")): self.response.out.write('{"success":2}') # check if user exists
-            elif not functions.has_crush(carleton_id, self.request.get("to")): self.response.out.write('{"success":3}') # you must have them as a crush
+            source = session.getCarl()
+            target = functions.get_user_by_CID(self.request.get("to"))
+            if not target: self.response.out.write('{"success":2}') # check if user exists
+            elif not functions.has_crush(source, target): self.response.out.write('{"success":3}') # you must have them as a crush
             else:
-                message = models.Message()
-                message.source = carleton_id
-                message.target = self.request.get("to")
-                message.message = self.request.get("body")
-                message.put()
+                thread = models.Thread()
+                thread.source = source
+                thread.target = target
+                thread.message = self.request.get("body")
+                thread.put()
                 self.response.out.write('{"success":0}')
         else:
             self.response.out.write('{"success":1}')
@@ -20,11 +21,11 @@ class Send(webapp.RequestHandler):
 class Delete(webapp.RequestHandler):
     def post(self):
         if session.isPaired() and session.opted_in():
-            carleton_id = session.getCarl().carletonID
-            message = models.Message.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the message
-            if message and message.target == carleton_id: # users can only delete their own messages
-                message.deleted = True
-                message.put()
+            target = session.getCarl()
+            thread = models.Thread.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the thread
+            if thread and thread.target.carletonID == target.carletonID: # kind of weird that we need to compare their carletonIDs instead of just comparing them
+                thread.deleted = True
+                thread.put()
                 self.response.out.write('{"success":0}')
             else:
                 self.response.out.write('{"success":2}')
--- LCD/models.py
+++ LCD/models.py
@@ -10,12 +10,12 @@ class Carl(db.Model):
     opted_in = db.BooleanProperty(default=True)
 
 class Crush(db.Model):
-    source = db.ReferenceProperty(Carl, collection_name="source")
-    target = db.ReferenceProperty(Carl, collection_name="target")
+    source = db.ReferenceProperty(Carl, collection_name="crush_source")
+    target = db.ReferenceProperty(Carl, collection_name="crush_target")
 
-class Message(db.Model):
+class Thread(db.Model):
     deleted = db.BooleanProperty(default=False) 
-    source = db.StringProperty() # change to reference properties
-    target = db.StringProperty()
+    source = db.ReferenceProperty(Carl, collection_name="thread_source")
+    target = db.ReferenceProperty(Carl, collection_name="thread_target")
     message = db.StringProperty(multiline=True)
     created = db.DateTimeProperty(auto_now_add=True)
