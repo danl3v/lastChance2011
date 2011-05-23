6375bd898cb725003511f2d6fcfee2fb04aa538a
Daniel Levy <dlouislevy@gmail.com>
2011-05-22 19:22:43 -0500

    adding matches page

---
+++ LCD/controllers/matches.py
@@ -0,0 +1,14 @@
+from google.appengine.ext import webapp
+from models import models
+import session, view, functions
+
+class Matches(webapp.RequestHandler):
+    def get(self):
+        if session.isPaired() and session.opted_in():
+            template_values = {
+                'matches': session.getCarl().matches.fetch(10),
+                'current_page': { 'matches': True }
+                }
+            view.renderTemplate(self, 'matches.html', template_values)
+        else:
+            self.response.out.write('Your account must be paired and opted-in in order to view matches. Go to <a href="/settings">settings</a> to resolve this issue.')
