01a20d12ba833fe52e656024233a59351f1303e5
Daniel Levy <dlouislevy@gmail.com>
2011-05-22 22:36:27 -0500

    added decrators to crushes and messages

--- LCD/controllers/messages.py
+++ LCD/controllers/messages.py
@@ -5,8 +5,8 @@ import view, session, functions
 
 class Send(webapp.RequestHandler):
     @functions.only_if_site_open
+    @functions.only_if_paired_opted_in
     def post(self):
-        if session.isPaired() and session.opted_in():
             source = session.getCarl()
             target = functions.get_user_by_CID(self.request.get("to"))
             if not target: self.response.out.write('{"success":2}') # check if user exists
@@ -23,48 +23,42 @@ class Send(webapp.RequestHandler):
                 target.put()
 
                 self.response.out.write('{"success":0,"mid":' + str(message.key().id()) + ',"name":"' + message.target.first_name + ' ' + message.target.last_name + '"}')
-        else:
-            self.response.out.write('{"success":1}')
 
 class Reply(webapp.RequestHandler):
     @functions.only_if_site_open
+    @functions.only_if_paired_opted_in
     def post(self):
-        if session.isPaired() and session.opted_in():
-            message = models.Message.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the message
-            source = session.getCarl()
-            if message and (message.source.carletonID == source.carletonID or message.target.carletonID == source.carletonID):
-                message.updated = datetime.now()
-                message.source_deleted = False
-                message.target_deleted = False
-                message.unread = True
-                message.put()
-                #update target's unread count
-                otherPerson = message.target if message.source.carletonID == source.carletonID else message.source
-                otherPerson.has_unread_messages = True
-                otherPerson.put()
-                reply = models.Reply()
-                reply.message = message
-                reply.source = source
-                reply.body = self.request.get("body")
-                reply.put()
-                self.response.out.write('{"success":0}')
-            else:
-                self.response.out.write('{"success":2}')
+        message = models.Message.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the message
+        source = session.getCarl()
+        if message and (message.source.carletonID == source.carletonID or message.target.carletonID == source.carletonID):
+            message.updated = datetime.now()
+            message.source_deleted = False
+            message.target_deleted = False
+            message.unread = True
+            message.put()
+            #update target's unread count
+            otherPerson = message.target if message.source.carletonID == source.carletonID else message.source
+            otherPerson.has_unread_messages = True
+            otherPerson.put()
+            reply = models.Reply()
+            reply.message = message
+            reply.source = source
+            reply.body = self.request.get("body")
+            reply.put()
+            self.response.out.write('{"success":0}')
         else:
-            self.response.out.write('{"success":1}')
+            self.response.out.write('{"success":2}')
 
 class Delete(webapp.RequestHandler):
     @functions.only_if_site_open
+    @functions.only_if_paired_opted_in
     def post(self):
-        if session.isPaired() and session.opted_in():
-            user = session.getCarl()
-            message = models.Message.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the message
-            if message:
-                if user.carletonID == message.source.carletonID: message.source_deleted = True
-                elif user.carletonID == message.target.carletonID: message.target_deleted = True
-                message.put()
-                self.response.out.write('{"success":0}')
-            else:
-                self.response.out.write('{"success":2}')
+        user = session.getCarl()
+        message = models.Message.get_by_id(long(self.request.get("mid"))) # maybe use key instead of key.id to find the message
+        if message:
+            if user.carletonID == message.source.carletonID: message.source_deleted = True
+            elif user.carletonID == message.target.carletonID: message.target_deleted = True
+            message.put()
+            self.response.out.write('{"success":0}')
         else:
-            self.response.out.write('{"success":1}')
+            self.response.out.write('{"success":2}')
